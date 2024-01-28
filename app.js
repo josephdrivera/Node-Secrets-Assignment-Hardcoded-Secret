39366516bd6a05f35a59be8a7907e26885587e57
Joseph D Rivera <josephdrivera15@gmail.com>
2024-01-28 17:41:45 -0500

    Update secret key and port number

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const express = require('express');
 const app = express();
 
 // Define a hardcoded secret key
-const secretKey = 'mySecretKey123';
+const secretKey = 'ThisIsMySecretKey';
 
 // Define a route handler for the home page
 app.get('/', (req, res) => {
   res.send('Hello World! Your secret key is: ' + secretKey);
 });
 
-// Define the port to run the server on
-const PORT = 3000;
+
+const PORT = 6060;
 
 // Start the server
 app.listen(PORT, () => {
